369743d2bf4db58de9589806ce0bf0e266f82913
lizhizhou <lzz@meteroi.com>
2014-03-23 09:27:13 +0800

    Add trace start and end
    Add trace start and end

--- src/trace/trace.java
+++ src/trace/trace.java
@@ -9,14 +9,14 @@ public class trace {
 		String data = null;
 		try {		
 			File f;
-			boolean reverse_mode = false;
-			if(args[0].compareTo("-r") == 0)
-			{
-				f = new File(args[1]);	
-				reverse_mode = true;
-			} else {
-				f = new File(args[0]);
-			}
+			//boolean reverse_mode = false;
+			//if(args[0].compareTo("-r") == 0)
+			//{
+			//	f = new File(args[1]);	
+			//	reverse_mode = true;
+			//} else {
+			f = new File(args[0]);
+			//}
 			BufferedReader br = new BufferedReader(new FileReader(f));
 			String s;
 			StringBuilder sb = new StringBuilder();
@@ -40,7 +40,7 @@ public class trace {
 				Pattern p = Pattern.compile(regx);
 				Matcher m = p.matcher(data);	
 		        if (m.find()) {  
-		        	data = m.replaceAll(")\n{TRACE");  
+		        	data = m.replaceAll(")\n{TRACE_START");  
 		        }  
 			}
 			{
@@ -49,7 +49,7 @@ public class trace {
 				Pattern p = Pattern.compile(regx);
 				Matcher m = p.matcher(data);	
 		        if (m.find()) {  
-		        	data = m.replaceAll("TRACE}");    
+		        	data = m.replaceAll("TRACE_END}");    
 		        }  
 			}
 			{
@@ -60,12 +60,13 @@ public class trace {
 			    StringBuffer sb1 = new StringBuffer();  
 			    while (m.find())  
 			    {  
-			      m.appendReplacement(sb1, "{TRACE return" + m.group(1) + ";}");       
+			      m.appendReplacement(sb1, "{TRACE_END return" + m.group(1) + ";}");       
 			    }  
 			    m.appendTail(sb1);  
 			    data = sb1.toString();
 			}
-			data = "#define TRACE {buginf(\"\\n%s %s %d %d\",__FILE__,__FUNCTION__,__LINE__, clock_get_microsecs());}\n"
+			data = "#define TRACE_START {buginf(\"\\nSTART %s %s %d %d\",__FILE__,__FUNCTION__,__LINE__, clock_get_microsecs());}\n"
+			+"#define TRACE_END {buginf(\"\\END n%s %s %d %d\",__FILE__,__FUNCTION__,__LINE__, clock_get_microsecs());}\n"
 			+ data;
 			System.out.println(data);
 			FileWriter outfile=new FileWriter(f);
